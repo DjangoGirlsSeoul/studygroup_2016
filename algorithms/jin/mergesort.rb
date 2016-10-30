3b58b7aa079e51d6344180cd9a4c601fff9d9d3e
Jin Park <jin@jinpark.net>
2016-10-30 14:22:15 +0900

    Update mergesort.rb

--- algorithms/jin/mergesort.rb
+++ algorithms/jin/mergesort.rb
@@ -1,14 +1,18 @@
 def mergesort(array)
+    # base case
     return array if array.length < 2
 
     mid = array.length / 2
+    # three dots(...) doesnt take the last value
     sorted_left = mergesort(array[0...mid])
+    # two dots(..) does take the last value
     sorted_right = mergesort(array[mid..array.length])
     
     merge(sorted_left, sorted_right)
 end
 
 def merge(left, right)
+    # ruby is magic! (empty? is same as array.length == 0)
     if left.empty?
         right
     elsif right.empty?
@@ -18,5 +22,4 @@ def merge(left, right)
     else
         [right.first] + merge(left,right[1..right.length])
     end
-
 end
