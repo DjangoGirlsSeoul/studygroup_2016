6a78f66b57d5c0e491c3cd45634fc4dc4b11ae1d
mingrammer <k239507@gmail.com>
2016-11-02 23:37:45 +0900

    Cleanup the mergesort source codes

--- algorithms/code/mingrammer/mergesort.py
+++ algorithms/code/mingrammer/mergesort.py
@@ -4,8 +4,8 @@ def mergesort(arr):
     if size == 1:
         return arr
 
-    l_arr = mergesort(arr[:int(size/2)])
-    r_arr = mergesort(arr[int(size/2):])
+    l_arr = mergesort(arr[:size // 2])
+    r_arr = mergesort(arr[size // 2:])
 
     l_size = len(l_arr)
     r_size = len(r_arr)
@@ -14,19 +14,15 @@ def mergesort(arr):
 
     merged = []
 
-    for k in range(size):
-        if i == l_size:
-            merged.append(r_arr[j])
-            j += 1
-        elif j == r_size:
+    while i < l_size and j < r_size:
+        if l_arr[i] < r_arr[j]:
             merged.append(l_arr[i])
             i += 1
         else:
-            if l_arr[i] <= r_arr[j]:
-                merged.append(l_arr[i])
-                i += 1
-            else:
-                merged.append(r_arr[j])
-                j += 1
+            merged.append(r_arr[j])
+            j += 1
+
+    merged.extend(l_arr[i:])
+    merged.extend(r_arr[j:])
 
     return merged
